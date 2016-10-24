352481f618d3a1a631982028a63dbc0e3e96f2cc
Scott.Lee <once_key@126.com>
2016-10-24 19:00:29 +0800

    移除node_modules文件夹版本控制

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import { Button } from 'antd';
+import {Rate} from 'antd';
+
+ReactDOM.render(
+  <div>
+    <h1>SaaS管理台</h1>
+    <Rate/>
+ 	<Button type="primary" size="large">Large</Button>
+  </div>,
+  document.getElementById('app')
+);
